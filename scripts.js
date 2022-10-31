d960d70740f466084b9684ccbf50d143256c0047
Romario2Crosbie <romario.anonymous2000@gmail.com>
2022-10-30 21:48:36 -0500

    completed webpage layout

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+/*window.onload = function() {
+    const btn1 = document.getElementsByClassName("btn");
+    const msg = document.getElementsByClassName("message");
+    function outp(){
+        msg.innerText = msg;
+    }
+    function validate(){
+        var email = document.getElementById("email").value;
+
+        var regExp = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
+        btn1.addEventListener('click', outp);
+        if (validate()){
+            
+            return true
+        }
+        else{
+            alert("Please enter a valid email address.");
+            return false
+        }
+    }
+
+}*/
